4dea1bec1e355e2f244ca06e635c38746df80451
Рунчев Руслан Павлович <ruslan.runchev@metinvestholding.com>
2021-07-12 16:04:41 +0300

    Add project files.

---
+++ BabylonJS/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace BabylonJS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BabylonJS/Scripts/app.js
@@ -0,0 +1,96 @@
+﻿var App = (function () {
+
+    var scene;
+    var engine;
+    var cars;
+
+    function _init() {
+
+        _load();
+    }
+
+    function _initBabylon() {
+
+        var canvas = document.getElementById('canvas');
+
+        engine = new BABYLON.Engine(canvas, true);
+
+        scene = _createScene();
+
+        engine.runRenderLoop(function () {
+            scene.render();
+        });
+
+    }
+
+    function _load() {
+
+        $.ajax({
+            url: '/Content/cars.json'
+        })
+            .done(function (_cars) {
+                cars = _cars;
+                _initBabylon();
+            });
+    }
+
+
+    function _createScene() {
+
+        var scene = new BABYLON.Scene(engine);
+
+        scene.clearColor = BABYLON.Color3.White();
+
+        var camera = new BABYLON.ArcRotateCamera("Camera",
+            BABYLON.Tools.ToRadians(0),
+            BABYLON.Tools.ToRadians(0),
+            100,
+            BABYLON.Vector3.Zero());
+
+        camera.panningSensibility = 50;
+
+        camera.attachControl(canvas, true);
+
+        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 100, 0), scene);
+
+        light.intensity = 1;
+
+        BABYLON.SceneLoader.ImportMesh(
+            '',
+            '/Content/',
+            'car.glb',
+            scene,
+            function (newMeshes) {
+
+                var car = newMeshes[0];
+
+                _drawCars(car);
+
+            });
+
+        return scene;
+    }
+
+    function _drawCars(_car) {
+
+        $.each(cars.ways, function (wayIndex, way) {
+
+            $.each(way.cars, function (carIndex, car) {
+
+                var newCar = _car.clone('car' + wayIndex + carIndex);
+
+                newCar.position.x = wayIndex * 10;
+                newCar.position.z = carIndex * 6;
+
+            });
+
+        });
+
+        scene.removeMesh(_car);
+    }
+
+    return {
+        init: _init
+    }
+
+})();
